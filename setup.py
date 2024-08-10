fdf4cec172ced8b6d353d453e9c2e42c5fdea8be
gripka <gripkagripka@gmail.com>
2024-08-10 12:02:40 -0300

    setup update dll

--- setup.py
+++ setup.py
@@ -1,10 +1,26 @@
 import sys
 from cx_Freeze import setup, Executable
+import os
+
+# Caminho para a pasta onde as DLLs estão localizadas
+dlls_folder = os.path.join('libs')
+
+def copy_dlls():
+    import shutil
+    import glob
+    
+    build_dir = 'build/exe.win-amd64-3.12'  # Ajuste conforme necessário
+    dll_files = glob.glob(os.path.join(dlls_folder, '*.dll'))
+    for dll in dll_files:
+        shutil.copy(dll, build_dir)
 
 build_exe_options = {
     "packages": ["tkinter", "tkcalendar"],
     "includes": ["impressao", "gui"],  # Liste os módulos sem a extensão .py
-    "include_files": [("icons/icogerenciamento2.ico", "icons/icogerenciamento2.ico")],
+    "include_files": [
+        ("icons/icogerenciamento2.ico", "icons/icogerenciamento2.ico"),
+        (dlls_folder, 'libs')  # Inclua a pasta de DLLs
+    ],
 }
 
 base = None
@@ -23,3 +39,6 @@ setup(
         icon="icons/icogerenciamento2.ico"
     )]
 )
+
+# Copie as DLLs após o build
+copy_dlls()
